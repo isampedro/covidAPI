556bb7e9a7ecf255087eb502f9eced6d482e5ea4
Manuel Luque Meijide <37815318+maanuluque@users.noreply.github.com>
2021-12-20 19:08:19 -0300

    Python script to import csv to mongoDB

---
+++ import.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pymongo import MongoClient
+import json
+
+def main():
+    print(str(mongoimport("./COVID.csv", "covid", "tweets")) + " documents were added.")
+
+def mongoimport(csv_path, db_name, coll_name, db_url='localhost', db_port=27017):
+    """ Imports a csv file at path csv_name to a mongo colection
+    returns: count of the documants in the new collection
+    """
+    client = MongoClient(db_url, db_port)
+    db = client[db_name]
+    coll = db[coll_name]
+    data = pd.read_csv(csv_path)
+    payload = json.loads(data.to_json(orient='records'))
+    coll.delete_many({})
+    coll.insert_many(payload)
+    return coll.count_documents({})
+
+if __name__ == "__main__":
+    main()
